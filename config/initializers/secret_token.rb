bbf3bf22b1baa7e5659d976be2e24f3457cbad27
Ludovic Henry <ludovichenry.utbm+git@gmail.com>
2012-11-23 21:04:30 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Achieveit::Application.config.secret_token = '3874060e3ac40c0499294c1432a697a319213462982072ff033f32babf57ef0600a5f31eadbfac15ed318a2b03e86da9c1cf0ffe84ea52b50b32cd5c3e215f4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Achieveit::Application.config.session_store :cookie_store, key: '_achieveit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Achieveit::Application.config.session_store :active_record_store
