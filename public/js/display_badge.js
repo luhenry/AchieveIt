335679e8e6c2a74ab961430c7fd5676a2cc61f86
FELD Boris <lothiraldan@gmail.com>
2012-11-24 23:36:35 +0100

    Frontend ++

--- public/js/display_badge.js
+++ public/js/display_badge.js
@@ -1,59 +1,64 @@
-function request(url, postData, callback, error_callback)
-{
-    var XMLHttpFactories = [
-        function () {return new XMLHttpRequest()},
-        function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-        function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-        function () {return new ActiveXObject("Microsoft.XMLHTTP")}
-    ];
+(function(scope){
 
-    function createXMLHTTPObject() {
-        var xmlhttp = false;
-        for (var i=0;i<XMLHttpFactories.length;i++) {
-            try {
-                xmlhttp = XMLHttpFactories[i]();
+    function request(url, method, callback, error_callback) {
+        var XMLHttpFactories = [
+            function () {return new XMLHttpRequest()},
+            function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+            function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+            function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+        ];
+
+        function createXMLHTTPObject() {
+            var xmlhttp = false;
+            for (var i=0;i<XMLHttpFactories.length;i++) {
+                try {
+                    xmlhttp = XMLHttpFactories[i]();
+                }
+                catch (e) {
+                    continue;
+                }
+                break;
             }
-            catch (e) {
-                continue;
-            }
-            break;
+            return xmlhttp;
         }
-        return xmlhttp;
+
+        var req = createXMLHTTPObject();
+        if (!req) return;
+
+        console.log(method);
+        req.open("GET",url);
+
+        req.onreadystatechange = function () {
+            if (req.readyState != 4) return;
+            if (req.status != 200 && req.status != 304) {
+                error_callback(req);
+            }
+            callback(req);
+        }
+
+        req.send("");
     }
 
-    var req = createXMLHTTPObject();
-    if (!req) return;
-    var method = (postData) ? "POST" : "GET";
 
-    req.open(method,url,true);
-
-    if(postData)
-        req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-
-    req.onreadystatechange = function () {
-        if (req.readyState != 4) return;
-        if (req.status != 200 && req.status != 304) {
-            error_callback(req);
-        }
-        callback(req);
+    function show_badge(api_key, project_slug, achievement_slug)
+    {
+        console.log("Coucou");
+        request('me.json?auth_token=' + api_key, 'GET', function(req) {
+            console.log('req' + req);
+        })
+        // request('user_achievement/?auth_key=' + api_key +
+        //     '&project_slug=' + project_slug +
+        //     '&achievement_slug=' + achievement_slug, 'GET',
+        //     function (req) {
+        //         console.log(req);
+        //         // Show data
+        //     }, function (req) {
+        //         if(req.status == 401) {
+        //             console.log('401');
+        //             // Show login button
+        //         }
+        //     });
     }
 
-    req.send(qdata);
-}
-
-
-function show_badge(api_key, project_slug, achivement_slug)
-{
-    request('http://api.gameup.co/user_achievement', {
-            api_key: api_key,
-            project_slug: project_slug,
-            achievement_slug: achievement_slug
-        }, function (req) {
-            // Show data
-        }, function (req) {
-            if(req.status == 401) {
-                console.log('401');
-                // Show login button
-            }
-        });
-}
+    scope.show_badge = show_badge;
+})(window);
